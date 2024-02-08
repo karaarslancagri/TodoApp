a22f35ce67a24c724ccbc2e72af2419cf81491fc
Çağrı Karaarslan <karaarslancagri1@gmail.com>
2024-02-08 21:32:25 +0300

    card fixed

---
+++ lib/constans/color.dart
@@ -0,0 +1,2 @@
+const String headerColor = "#4A3780";
+const String baseColor = "#E0E0E0";
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:todo_app/constans/color.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isChecked = false;
+  @override
   Widget build(BuildContext context) {
+    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidht = MediaQuery.of(context).size.width;
+
     return MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: HexColor(baseColor),
+          body: Column(children: [
+            Container(
+              width: deviceWidht,
+              height: deviceHeight / 3,
+              color: HexColor(headerColor),
+              child: const Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 30,
+                    ),
+                    child: Text(
+                      "October 20,2022",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 40,
+                    ),
+                    child: Text(
+                      "My Todo List",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 44,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: Column(
+                children: [
+                  Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Icon(
+                            Icons.notes_outlined,
+                            size: 50,
+                          ),
+                          const Text(
+                            "Study Lessons",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Checkbox(
+                              value: isChecked,
+                              onChanged: (val) => {
+                                    setState(() {
+                                      isChecked = val!;
+                                    })
+                                  })
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
     );
   }
 }
